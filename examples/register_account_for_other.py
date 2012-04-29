e1c771dec056fa0aff5d12375a02c58e33590ba3
Steven Lehrburger <lehrburger@gmail.com>
2012-04-29 01:52:47 -0400

    Example of in-band registration of user by admin
    XEP-0077 provides a specification for the in-band creation of new users, and this is usually done by an unauthenticated client (since the user who will be authenticating does not yet exist). It may be desirable in a client application to allow *only* authenticated administrators (or bots with administrative privileges) to create new accounts on behalf of other users.
    
    This example, when paired with the appropriate modifications on the server, demonstrates how this works.
    
    I probably wouldn't have gotten this working without legastero's help here:
    	http://groups.google.com/group/sleekxmpp-discussion/browse_thread/thread/f2652563db0def11
    
    The corresponding server changes for ejabberd are here:
    	https://github.com/lehrblogger/ejabberd/commit/d58f21a404aef6394a193ee8c3320a08eb2f6e1b
    
    The following links may also be useful context:
    	http://www.ejabberd.im/node/5020
    	http://www.ejabberd.im/node/1211
    	http://yo.jabber.ru/bugzilla/show_bug.cgi?id=300
    	http://stackoverflow.com/questions/7709658/asmack-xmpp-new-user-registration/7725400

---
+++ examples/register_account_for_other.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    SleekXMPP: The Sleek XMPP Library
+    Copyright (C) 2010  Nathanael C. Fritz
+    This file is part of SleekXMPP.
+
+    See the file LICENSE for copying permission.
+"""
+
+import sys
+import logging
+import getpass
+from optparse import OptionParser
+
+import sleekxmpp
+from sleekxmpp.exceptions import IqError, IqTimeout
+
+# Python versions before 3.0 do not use UTF-8 encoding
+# by default. To ensure that Unicode is handled properly
+# throughout SleekXMPP, we will set the default encoding
+# ourselves to UTF-8.
+if sys.version_info < (3, 0):
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+else:
+    raw_input = input
+
+
+class RegisterBot(sleekxmpp.ClientXMPP):
+
+    """
+    A basic bot that will attempt to register an account
+    with an XMPP server.
+
+    NOTE: This follows the very basic registration workflow
+          from XEP-0077. More advanced server registration
+          workflows will need to check for data forms, etc.
+    """
+
+    def __init__(self, jid, password, new_jid, new_password):
+        sleekxmpp.ClientXMPP.__init__(self, jid, password)
+        self.new_jid = new_jid
+        self.new_password = new_password
+
+        # The session_start event will be triggered when
+        # the bot establishes its connection with the server
+        # and the XML streams are ready for use. We want to
+        # listen for this event so that we we can initialize
+        # our roster.
+        self.add_event_handler("session_start", self.start)
+
+    def start(self, event):
+        """
+        Process the session_start event.
+
+        Typical actions for the session_start event are
+        requesting the roster and broadcasting an initial
+        presence stanza.
+
+        Arguments:
+            event -- An empty dictionary. The session_start
+                     event does not provide any additional
+                     data.
+        """
+        
+        self.send_presence()
+        self.get_roster()
+        
+        iq = self.Iq()
+        iq['type'] = 'set'
+        iq['to'] = self.boundjid.domain
+        iq['register']['username'] = self.new_jid
+        iq['register']['password'] = self.new_password
+        
+        try:
+            iq.send()
+            logging.info("Account created for %s!" % self.boundjid)
+        except IqError as e:
+            logging.error("Could not register account: %s" % e.iq['error']['text'])
+        except IqTimeout:
+            logging.error("No response from server.")
+        
+        self.disconnect()
+        
+
+if __name__ == '__main__':
+    # Setup the command line arguments.
+    optp = OptionParser()
+
+    # Output verbosity options.
+    optp.add_option('-q', '--quiet', help='set logging to ERROR',
+                    action='store_const', dest='loglevel',
+                    const=logging.ERROR, default=logging.INFO)
+    optp.add_option('-d', '--debug', help='set logging to DEBUG',
+                    action='store_const', dest='loglevel',
+                    const=logging.DEBUG, default=logging.INFO)
+    optp.add_option('-v', '--verbose', help='set logging to COMM',
+                    action='store_const', dest='loglevel',
+                    const=5, default=logging.INFO)
+
+    # JID and password options.
+    optp.add_option("-j", "--jid", dest="jid",
+                    help="JID to use")
+    optp.add_option("-p", "--password", dest="password",
+                    help="password to use")
+
+    optp.add_option("-n", "--new_jid", dest="new_jid",
+                        help="the new jid to register")
+                        
+    optp.add_option("-w", "--new_password", dest="new_password",
+                        help="the new password for the new user")
+                        
+    opts, args = optp.parse_args()
+
+    # Setup logging.
+    logging.basicConfig(level=opts.loglevel,
+                        format='%(levelname)-8s %(message)s')
+
+    if opts.jid is None:
+        opts.jid = raw_input("Username: ")
+    if opts.password is None:
+        opts.password = getpass.getpass("Password: ")
+    if opts.new_jid is None:
+        opts.new_jid = raw_input("New username to register: ")
+    if opts.new_password is None:
+        opts.new_password = getpass.getpass("New password for new username: ")
+
+    # Setup the RegisterBot and register plugins. Note that while plugins may
+    # have interdependencies, the order in which you register them does
+    # not matter.
+    xmpp = RegisterBot(opts.jid, opts.password, opts.new_jid, opts.new_password)
+    xmpp.register_plugin('xep_0030') # Service Discovery
+    xmpp.register_plugin('xep_0004') # Data forms
+    xmpp.register_plugin('xep_0066') # Out-of-band Data
+    xmpp.register_plugin('xep_0077') # In-band Registration
+
+    # If you are working with an OpenFire server, you may need
+    # to adjust the SSL version used:
+    # xmpp.ssl_version = ssl.PROTOCOL_SSLv3
+
+    # If you want to verify the SSL certificates offered by a server:
+    # xmpp.ca_certs = "path/to/ca/cert"
+
+    # Connect to the XMPP server and start processing XMPP stanzas.
+    if xmpp.connect(('127.0.0.1', 5222)):
+        # If you do not have the dnspython library installed, you will need
+        # to manually specify the name of the server if it does not match
+        # the one in the JID. For example, to use Google Talk you would
+        # need to use:
+        #
+        # if xmpp.connect(('talk.google.com', 5222)):
+        #     ...
+        xmpp.process(block=True)
+        print("Done")
+    else:
+        print("Unable to connect.")
